1632c337e8f0002051bd42afd5d24dd282eb5b4e
alexciccarese <alex.ciccarese@gmail.com>
2025-03-12 15:08:55 +0100

    add array posts

--- server.js
+++ server.js
@@ -10,3 +10,43 @@ app.get('/', (req, res) => {
   
   res.send('Server del mio blog')
 })
+
+const posts = [
+  {
+    id: 1,
+    title: 'Ciambellone',
+    content: 'Soft anf fluffy cake',
+    image: './images/ciambellone.jpeg',
+    tags: ['easy-recipe', 'homemade', 'fluffy', 'breakfast', 'snack']
+  },
+  {
+    id: 2,
+    title: 'Cracker barbabietola',
+    content: 'Crispy and flavorful cracker',
+    image: './images/cracker_barbabietola.jpeg',
+    tags: ['healthy-snack', 'homemade', 'crispy', 'vegan', 'gluten-free']
+  },
+  {
+    id: 3,
+    title: 'Pane fritto dolce',
+    content: 'Golden and crispy sweet fried bread',
+    image: './images/pane_fritto_dolce.jpeg',
+    tags: ['sweet-treat', 'crispy', 'homemade', 'breakfast', 'dessert']
+  },
+  {
+    id: 4,
+    title: 'Pasta barbabietola',
+    content: 'Vibrant and delicious beetroot pasta',
+    image: './images/pasta_barbabietola.jpeg',
+    tags: ['homemade', 'colorful', 'healthy', 'vegetarian', 'dinner']
+  },
+  {
+    id: 5,
+    title: 'Torta paesana',
+    content: 'A rich and rustic bread pudding cake',
+    image: './images/torta_paesana.jpeg',
+    tags: ['traditional', 'italian-dessert', 'rustic', 'chocolate', 'homemade']
+  },
+]
+
+console.log(posts);
