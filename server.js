30e55a6ba90bf0eb11e2f336d08d53f20a9db8ac
alexciccarese <alex.ciccarese@gmail.com>
2025-03-13 16:34:46 +0100

    add router file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3002
+const postsRouter = require('./routers/posts')
 
 //Configuriamo gli assets statici per visualizzare le immagini di ogni post
 app.use(express.static('public'))
@@ -53,8 +54,10 @@ const posts = [
 ]
 
 
-// root bacheca
+/* // root bacheca
 app.get('/api/v1/bacheca', (req, res) => {
 
   res.json(posts)
-})
+}) */
+
+app.use("/posts", postsRouter)
